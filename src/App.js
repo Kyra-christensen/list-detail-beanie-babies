d40d1fa81a94294d7afb7ff5a9e53982bd104603
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-07 12:06:34 -0800

    fixed spacing issue

--- src/App.js
+++ src/App.js
@@ -11,13 +11,24 @@ export default function App() {
   return (
     <Router>
       <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <link to="/">Home</link>
+            </li>
+            <li>
+              <link to="/detail">Home</link>
+            </li>
+          </ul>
+        </nav>
         <Switch>
-          <Route>
+          <Route exact path="/home">
             {/* this home page route should list all the beanies */}
-            
+            <BeaniesPage />
           </Route>
-          <Route> 
+          <Route exact path="/detail"> 
             {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+            <BeanieDetail />
           </Route>
         </Switch>
       </div>
