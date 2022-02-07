98fda4b5d67216d3d5da70cddd73297c22cf296d
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-07 12:11:46 -0800

    fixed path to the beanie detail

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Link
 } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
@@ -14,20 +15,20 @@ export default function App() {
         <nav>
           <ul>
             <li>
-              <link to="/">Home</link>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <link to="/detail">Home</link>
+              <Link to="/beanie-babies">Search</Link>
             </li>
           </ul>
         </nav>
         <Switch>
-          <Route exact path="/home">
+          <Route exact path="/">
             {/* this home page route should list all the beanies */}
             <BeaniesPage />
           </Route>
-          <Route exact path="/detail"> 
-            {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+          <Route exact path="/beanie-babies/:id"> 
+            {/* this route should point to a particular beanie baby by id and render that specific BeanieDetail page */}
             <BeanieDetail />
           </Route>
         </Switch>
