3c1644ef698dfde47afdd74ef9a0c0d381e9c107
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-07 12:32:30 -0800

    created beaniesList function that maps through all the beanie babies

--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -4,5 +4,6 @@ export default function BeaniesList({ beanieBabies }) {
   return (
     <div className='beanies'>
       {/* render out the beanie babies as a list */}
+      {beanieBabies.map((beanie, i) => <BeanieBaby key={`${beanie}-${i}`} beanieBaby={beanie} />)}
     </div>);
 }
